e0d24f212ca34513c41895b0b5ea20e9756ec40c
BT_1N3_31 <vzhao2047@bths.edu>
2018-11-20 09:47:55 -0500

    3

--- src/FrogRunner.java
+++ src/FrogRunner.java
@@ -6,9 +6,7 @@ public class FrogRunner
         FrogSimulation frog2 = new FrogSimulation(50,10);
         FrogSimulation frog3 = new FrogSimulation(15,2);
 
-        System.out.println("Frog 1 Success Rate: "+ frog1.runSimulations(5)
-
-        );
+        System.out.println("Frog 1 Success Rate: "+ frog1.runSimulations(5));
         System.out.println("Frog 2 Success Rate: "+ frog2.runSimulations(5));
         System.out.println("Frog 3 Success Rate: "+ frog3.runSimulations(5));
     }
